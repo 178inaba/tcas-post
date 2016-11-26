5d6de3f9567d60f4b1df665b75c2cff976d9b9d2
178inaba <178inaba@users.noreply.github.com>
2016-11-27 03:29:04 +0900

    Add poster.

--- main.go
+++ main.go
@@ -1,79 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/178inaba/twitcasting"
+	"github.com/178inaba/tcpost/poster"
 	log "github.com/Sirupsen/logrus"
-	"github.com/howeyc/gopass"
 	"github.com/urfave/cli"
 )
 
-const (
-	failExitStatusCode = 1
-)
-
-type poster struct {
-	client *twitcasting.Client
-}
-
 func main() {
-	var username, password string
-
-	// Login username from stdin.
-	fmt.Print("Username: ")
-	fmt.Scanln(&username)
-
-	// Password from stdin.
-	fmt.Print("Password: ")
-	pBytes, err := gopass.GetPasswd()
-	if err != nil {
-		log.Fatalf("Input Password error: %v.", err)
-	}
-
-	password = string(pBytes)
-
-	client, err := twitcasting.NewClient(username, password)
-	if err != nil {
-		log.Fatalf("NewClient error: %v.", err)
-	}
-
-	p := &poster{client: client}
+	p, err := poster.NewPoster()
 
 	app := cli.NewApp()
 	app.Name = "tcpost"
 	app.HelpName = app.Name
 	app.Usage = "Post comment to TwitCasting."
 	app.Version = "1.0.0"
-	app.Action = p.action
+	app.Action = p.Action
 
 	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatalf("Run error: %v", err)
 	}
-}
-
-func (p *poster) action(c *cli.Context) error {
-	target := c.Args().Get(0)
-	comment := c.Args().Get(1)
-
-	err := p.client.Auth()
-	if err != nil {
-		return cli.NewExitError(err.Error(), failExitStatusCode)
-	}
-
-	movieID, err := p.client.GetMovieID(target)
-	if err != nil {
-		return cli.NewExitError(err.Error(), failExitStatusCode)
-	}
-
-	err = p.client.PostComment(comment, target, movieID)
-	if err != nil {
-		return cli.NewExitError(err.Error(), failExitStatusCode)
-	}
-
-	log.Infof("PostComment success!: %s", comment)
-
-	return nil
 }
---
+++ poster/poster.go
@@ -0,0 +1,72 @@
+package poster
+
+import (
+	"fmt"
+
+	"github.com/178inaba/twitcasting"
+	log "github.com/Sirupsen/logrus"
+	"github.com/howeyc/gopass"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli"
+)
+
+const (
+	failExitStatusCode = 1
+)
+
+// Poster is ...
+type Poster struct {
+	client *twitcasting.Client
+}
+
+// NewPoster is ...
+func NewPoster() (*Poster, error) {
+	var username, password string
+
+	// Login username from stdin.
+	fmt.Print("Username: ")
+	fmt.Scanln(&username)
+
+	// Password from stdin.
+	fmt.Print("Password: ")
+	pBytes, err := gopass.GetPasswd()
+	if err != nil {
+		return nil, errors.Wrap(err, "input password error")
+	}
+
+	password = string(pBytes)
+
+	client, err := twitcasting.NewClient(username, password)
+	if err != nil {
+		return nil, errors.Wrap(err, "new twitcasting client error")
+	}
+
+	p := &Poster{client: client}
+
+	return p, nil
+}
+
+// Action is ...
+func (p *Poster) Action(c *cli.Context) error {
+	target := c.Args().Get(0)
+	comment := c.Args().Get(1)
+
+	err := p.client.Auth()
+	if err != nil {
+		return cli.NewExitError(err.Error(), failExitStatusCode)
+	}
+
+	movieID, err := p.client.GetMovieID(target)
+	if err != nil {
+		return cli.NewExitError(err.Error(), failExitStatusCode)
+	}
+
+	err = p.client.PostComment(comment, target, movieID)
+	if err != nil {
+		return cli.NewExitError(err.Error(), failExitStatusCode)
+	}
+
+	log.Infof("Post comment: %s", comment)
+
+	return nil
+}
